7b2ff282d45f5e30a198578a3846b09ab78d7358
范端端 <1208674494@qq.com>
2019-03-07 23:35:52 +0800

    complete branch.js

--- script/branch.js
+++ script/branch.js
@@ -1,16 +1,34 @@
-const shell = require('shelljs');
+const { exec,exit } = require('./helpers');
+const chalk = require('chalk');
 const version = require('../package').version;
 
+function localHasChange(){
+	if(exec('git status --porcelain').stdout.trim()){
+		console.log(chalk.red('❌ 本地有文件修改，请先完成commit或checkout！'));
+		exit(1);
+	};
+}
+localHasChange();
+
+
+function localHaveUpdate(){
+	if(exec('git pull --porcelain').stdout.trim()){
+		console.log(chalk.red('❌ 本地有文件修改，请先完成commit或checkout！'));
+		exit(1);
+	};
+}
+
 function isMaster() {
-	if(process.env.NODE_BRANCH !== 'master') {
+	if(exec.exec('git branch').stdout !== '* master') {
 		console.error('请先合并branch到master分支！');
 		return false;
 	}
 	return true;
 }
+
 module.exports = function() {
 	if(!isMaster()) return false;
-	if(shell.exec(`git checkout branch ${daily/version}`) !== 0) {
+	if(exec(`git checkout branch ${daily/version}`) !== 0) {
 		console.log(1);
 	};
 }
---
+++ script/helpers/index.js
@@ -0,0 +1,11 @@
+const shell = require('shelljs');
+
+let _exec_ = shell.exec;
+
+exports.exec = function(){
+	return _exec_(...[].slice.call(arguments))
+}
+
+exports.exit = function(code){
+	shell.exit(code);
+}
