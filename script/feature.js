02c64ac9824e2e03c88289f177583ab2d91be2e4
一木 <1208674494@qq.com>
2019-04-19 21:26:29 +0800

    feture

--- script/feature.js
+++ script/feature.js
@@ -2,14 +2,26 @@
 
 const { exec,exit,haveChange,getCurentBranchName } = require('./helpers');
 const chalk = require('chalk');
+let featurePre = 'feature/';
 
 function getArgs(){
 	return [].slice.apply(process.argv);
 }
 
-getArgs();
 
 module.exports = function() {
-
-	console.log(getArgs());
+	let featureName = getArgs()[2];
+	// feature 以feature/开头；
+	// 检验传入参数是否feture/开头，若是以它开头则直接使用；
+	if(getArgs){
+	  let preName = featureName.slice(0,8);//
+		if(featureName !== featurePre){
+			featureName = featurePre + featureName;
+		}
+		console.log(featureName,'232');
+		exec(`git checkout develop && git checkout -b ${featureName}`);
+	}else{
+		console.log(chalk.red('❌ 请输入feature分支名称！'));
+		exit(1);
+	}
 }
